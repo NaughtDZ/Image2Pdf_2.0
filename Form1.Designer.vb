26739b85fd0c5afbcf8e001b278b56272d722505
NaughtDZ <1603705420@qq.com>
2020-12-31 09:34:00 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Conver = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(93, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(232, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(93, 39)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(232, 21)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(4, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 20)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "图片文件夹"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(4, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(73, 21)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "输出文件"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Conver
+        '
+        Me.Conver.Location = New System.Drawing.Point(4, 66)
+        Me.Conver.Name = "Conver"
+        Me.Conver.Size = New System.Drawing.Size(73, 32)
+        Me.Conver.TabIndex = 4
+        Me.Conver.Text = "转换"
+        Me.Conver.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Enabled = False
+        Me.TrackBar1.Location = New System.Drawing.Point(4, 146)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(334, 45)
+        Me.TrackBar1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(91, 75)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "默认DPI"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(144, 72)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(31, 21)
+        Me.TextBox3.TabIndex = 7
+        Me.TextBox3.Text = "72"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(193, 77)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(132, 16)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "包括下一级子文件夹"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 104)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(313, 48)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "子文件夹选项"
+        Me.GroupBox1.Visible = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 20)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(143, 16)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "所有文件夹到一个文件"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(155, 20)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(143, 16)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "每个文件夹到单独文件"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(337, 183)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Conver)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Muti IMG2PDF"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Conver As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports System.IO '虽然写在这里，但是pdfsharp和sytem里都有Drawing等字类同名，请下面详写
+Imports PdfSharp
+Imports PdfSharp.Pdf
+Imports PdfSharp.Drawing
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog() '读图片文件夹
+        TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        TrackBar1.Value = 0
+    End Sub
+
+    Dim filelist() As String
+    Private Function GetAllImage(ByVal imgfold As String) '获取普通单独文件夹文件
+        Dim allfile() As String
+        Try
+            allfile = System.IO.Directory.GetFiles(imgfold)
+        Catch e As Exception
+            MsgBox(e.ToString)
+            Exit Function
+        End Try
+        Dim imgfile As Collection = New Collection
+        For Each things In allfile
+            If things.ToLower Like "*.png" Or things.ToLower Like "*.jpg" Or things.ToLower Like "*.jpeg" Or things.ToLower Like "*.bmp" Then
+                'Debug.WriteLine("筛选之后：" & things)
+                imgfile.Add(things)
+            End If
+        Next
+        Return imgfile
+    End Function
+
+    Private Function GetAllImageMuti(ByVal imgfold As String) '连带子文件夹,这个模式下生成直接在本过程里调用
+        Dim topfile() As String = GetAllImage(imgfold)
+        Dim alldir() As String = System.IO.Directory.GetDirectories(imgfold) '获取子文件夹
+        '这里要说明一下，判断放到循环外面肯定效率是高的
+        '把for each单独做个方法和这里复制两次其实是脱裤子放屁，况且一个全部合并生成pdf，一个是单独，还得传参还得判断，这还不如“冗余代码”，不如分两次
+        If RadioButton1.Checked = True Then
+            For Each nextdirs In alldir
+
+            Next
+        Else
+
+        End If
+
+        Return 1 '没卵用，也许可以用于debug
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveFileDialog1.ShowDialog()
+        TextBox2.Text = SaveFileDialog1.FileName
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Conver.Click
+        Conver.Enabled = False
+        TrackBar1.Value = 0
+        Dim w8t2con = GetAllImage(TextBox1.Text)
+        If w8t2con.Count = 0 Then
+            MsgBox("没有找到图片")
+            Conver.Enabled = True
+            Exit Sub
+        End If '直接有问题直接退出，不做分支了
+        Merg2pdf(w8t2con)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SaveFileDialog1.AddExtension = True
+        SaveFileDialog1.Filter = "PDF文件(*.pdf)|*.pdf"
+        Me.Height = 196
+        TrackBar1.Location = New Point(4, 110)
+    End Sub
+
+    Private Sub Merg2pdf(ByVal w8t2con)
+        TrackBar1.Maximum = w8t2con.Count
+        Dim document As PdfDocument = New PdfDocument  '创建pdf文件
+        For Each img In w8t2con
+            Dim page As PdfPage = document.AddPage() '创建新页
+            Dim gfx As XGraphics = XGraphics.FromPdfPage(page) '创建画布在page上
+            Dim ximg As XImage = XImage.FromFile(img) '创建gfx可用的image
+            'Dim g As Graphics
+            'g = Graphics.FromImage(Image.FromFile(img))
+            'Debug.WriteLine(ximg.HorizontalResolution) 'pdf打印页面大小与DPI有关
+            page.Width = ximg.PixelWidth '设置页面为图片分辨率,piexel是分辨率，width是通过dpi转换后的大小
+            page.Height = ximg.PixelHeight
+            'Debug.WriteLine("{0} x:{1} y:{2}", img, page.Width, page.Height)
+            'Debug.WriteLine("x:{0} y:{1}", g.DpiX / TextBox3.Text, g.DpiY / TextBox3.Text)
+            gfx.ScaleTransform(ximg.HorizontalResolution / TextBox3.Text)
+            gfx.DrawImage(ximg, 0, 0)
+            page.Close()
+            TrackBar1.Value += 1
+        Next
+        document.Save(TextBox2.Text)
+        Beep()
+        document.Close()
+        Conver.Enabled = True
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.CheckState = CheckState.Checked Then
+            GroupBox1.Visible = True
+            Me.Height = 222
+            TrackBar1.Location = New Point(4, 146)
+        Else
+            GroupBox1.Visible = False
+            Me.Height = 196
+            TrackBar1.Location = New Point(4, 110)
+        End If
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        Static temp As String
+        If RadioButton2.Checked = True Then
+            temp = TextBox2.Text
+            TextBox2.Text = "该模式下，PDF将会保存到输入目录下，文件名为目录名"
+        Else
+            TextBox2.Text = temp
+        End If
+    End Sub
+End Class
